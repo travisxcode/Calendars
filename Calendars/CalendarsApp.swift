464a7d7da832509ee780e4e3dbf91715494fecdb
trevisxcode <s.taeshawit@gmail.com>
2024-01-25 18:19:46 -0800

    Initial Commit

---
+++ Calendars/CalendarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarsApp.swift
+//  Calendars
+//
+//  Created by Taeshawit Suwanwigo on 25/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct CalendarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calendars/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calendars
+//
+//  Created by Taeshawit Suwanwigo on 25/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!/nCalendars")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
